5ccd5dedf4ce758fd1aacfb5e010eb37f41c0467
harshwardhannathmishra-boop <harshwardhannathmishra@gmail.com>
2025-09-10 22:17:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 https://yourusername.github.io/my-portfolio/
+console.log("Welcome to Rounak Mishra's portfolio!");
+
+window.onload = function() {
+    alert("Hello! Thanks for visiting my portfolio.");
+};
